596504881701fc2dd0703f0be226d06e01c973ac
iqbalkhan1997 <pathaniqbalkhan54321@gmail.com>
2022-02-27 21:32:26 +0530

    Added: equals method to check equality of two lengths

--- src/com/blz/oopsLineComparison/OopsLineComparison.java
+++ src/com/blz/oopsLineComparison/OopsLineComparison.java
@@ -14,8 +14,29 @@ public class OopsLineComparison {
         int y1 = input.nextInt();
         System.out.println("enter y2");
         int y2 = input.nextInt();
-        double lenOfLine = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
-        System.out.println("length of the line = " + lenOfLine);
 
+        System.out.println("Enter the second line coordinates");
+        System.out.println("enter p1");
+        int p1=input.nextInt();
+        System.out.println("enter q1");
+        int q1=input.nextInt();
+        System.out.println("enter p2");
+        int p2=input.nextInt();
+        System.out.println("enter q2");
+        int q2=input.nextInt();
+
+        Double firstLineLength = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+        System.out.println("length of the line = " + firstLineLength);
+
+        Double secondLineLength = Math.sqrt(Math.pow((p2 - p1), 2) + Math.pow((q2 - q1), 2));
+        System.out.println("length of the line = " + secondLineLength);
+
+        boolean equality = firstLineLength.equals(secondLineLength);
+        System.out.println(equality);
+        if(!equality){
+            System.out.println("Two lengths of the lines are not equal");
+        } else {
+            System.out.println("Two lengths of the lines are equal");
+        }
     }
 }
