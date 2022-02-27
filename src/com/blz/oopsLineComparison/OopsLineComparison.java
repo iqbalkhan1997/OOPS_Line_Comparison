1fb2435888ce79c01f699263c36d10681ebaf7f8
iqbalkhan1997 <pathaniqbalkhan54321@gmail.com>
2022-02-27 20:59:34 +0530

    Added welcome message

---
+++ src/com/blz/oopsLineComparison/OopsLineComparison.java
@@ -0,0 +1,8 @@
+package com.blz.oopsLineComparison;
+
+public class OopsLineComparison {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to line Comparison Computation Program");
+    }
+}
