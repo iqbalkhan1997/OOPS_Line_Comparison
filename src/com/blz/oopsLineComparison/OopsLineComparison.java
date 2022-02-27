92a05fd9bb956478ca61fe5c66438d1108069b08
iqbalkhan1997 <pathaniqbalkhan54321@gmail.com>
2022-02-27 21:58:45 +0530

    Used OOPS concept to calculate length,Equals and compareTo method

--- src/com/blz/oopsLineComparison/OopsLineComparison.java
+++ src/com/blz/oopsLineComparison/OopsLineComparison.java
@@ -1,44 +1,76 @@
 package com.blz.oopsLineComparison;
+
     import java.util.Scanner;
+
 public class OopsLineComparison {
-
-    public static void main(String[] args) {
-        System.out.println("Welcome to line Comparison Computation Program");
-        Scanner input=new Scanner(System.in);
-        System.out.println("Enter the line coordinates");
-        System.out.println("enter x1");
-        int x1 = input.nextInt();
-        System.out.println("enter x2");
-        int x2 = input.nextInt();
-        System.out.println("enter y1");
-        int y1 = input.nextInt();
-        System.out.println("enter y2");
-        int y2 = input.nextInt();
-
-        System.out.println("Enter the second line coordinates");
-        System.out.println("enter p1");
-        int p1=input.nextInt();
-        System.out.println("enter p2");
-        int p2=input.nextInt();
-        System.out.println("enter q1");
-        int q1=input.nextInt();
-        System.out.println("enter q2");
-        int q2=input.nextInt();
-
-        Double firstLineLength = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
-        System.out.println("length of the line = " + firstLineLength);
-
-        Double secondLineLength = Math.sqrt(Math.pow((p2 - p1), 2) + Math.pow((q2 - q1), 2));
-        System.out.println("length of the line = " + secondLineLength);
-
-        int compare = firstLineLength.compareTo(secondLineLength);
-        System.out.println(compare);
-        if(compare < 0){
-            System.out.println("First Line length is less than Second line");
-        } else if(compare > 0) {
-            System.out.println("First Line length is Greater than Second line");
-        } else {
-            System.out.println("Both first and second line lengths are equal");
+        double x1, x2, y1, y2;
+        Double line1,line2;
+        //initializing inputs of line coordinates from the user
+        public void coOrdinates(){
+            Scanner input=new Scanner(System.in);
+            System.out.println("enter x1");
+            x1 = input.nextInt();
+            System.out.println("enter x2");
+            x2 = input.nextInt();
+            System.out.println("enter y1");
+            y1 = input.nextInt();
+            System.out.println("enter y2");
+            y2 = input.nextInt();
         }
-    }
+        //method to calculate length of line
+        public Double length() {
+            Double lenOfLine=Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+            return lenOfLine;
+        }
+
+         //method to check equality of two lines
+         public void equal(Double length1,Double length2){
+              line1 = length1;
+              line2 = length2;
+              boolean equality = line1.equals(line2);
+              System.out.println(equality);
+              if(!equality){
+                  System.out.println("Two lengths of the lines are not equal");
+              } else {
+                  System.out.println("Two lengths of the lines are equal");
+              }
+        }
+    //method to compare two lengths of the lines
+        public void compare(Double length1,Double length2){
+            line1 = length1;
+            line2 = length2;
+            int equality = line1.compareTo(line2);
+            System.out.println(equality);
+            if(equality < 0){
+                System.out.println("First Line length is less than Second line");
+            } else if(equality > 0) {
+                System.out.println("First Line length is Greater than Second line");
+            } else {
+                System.out.println("Both first and second line lengths are equal");
+            }
+        }
+        //main method to behave/initialize methods and objects
+        public static void main(String[] args) {
+            System.out.println("Welcome to line Comparison Computation Program");
+            //initializing first line coordinates from the user
+            OopsLineComparison firstLine = new OopsLineComparison();
+            OopsLineComparison secondLine = new OopsLineComparison();
+            //initializing first line coordinates from the user
+            System.out.println("Enter the first line coordinates");
+            firstLine.coOrdinates();
+            //initializing second line coordinates from the use
+            System.out.println("Enter the second line coordinates");
+            secondLine.coOrdinates();
+            //printing out the first line length using length method
+            Double firstLineLength= firstLine.length();
+            System.out.println( "First line length is " + firstLineLength);
+            //printing out the second line length using length method
+            Double secondLineLength = secondLine.length();
+            System.out.println("Second line length is " + secondLineLength);
+            //initializing length of two lines to equals method
+            OopsLineComparison check = new OopsLineComparison();
+            check.equal(firstLineLength,secondLineLength);
+            //initializing length of two lines to compare method
+            check.compare(firstLineLength,secondLineLength);
+        }
 }
